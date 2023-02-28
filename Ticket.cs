61e88eaf529315cfa09987df71d105b2d23a771e
Rafael Valentini Taufer <117521021+zuzuwah@users.noreply.github.com>
2023-02-28 13:02:29 -0300

    Create Ticket.cs
    Exercício 01 da Semana 03

---
+++ Ticket.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M01S03
+{
+    public class Ticket
+    {
+        public DateTime Entrada { get; set; }
+
+        public DateTime Saida { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public Ticket(){}
+
+        public Ticket (DateTime entrada, DateTime saida, bool ativo){
+            this.Entrada = entrada;
+            this.Saida = saida;
+            this.Ativo = ativo;
+        }
+
+      
+
+        private double CalcularTempo()
+         {
+           TimeSpan tempo;
+            tempo = Saida - Entrada;
+            return tempo.TotalMinutes;
+
+         }
+
+         public double CalcularValor(){
+            double valor = CalcularTempo() * 1;
+            return valor;
+         }
+            
+    }
+
+}
